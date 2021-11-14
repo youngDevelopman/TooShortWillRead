a692b885e4425560958d6571a7e3973de6bfe346
Nazar Mazuryk <Nazar_Mazuryk@epam.com>
2021-11-14 15:53:47 +0200

    Create wikipedia data source

---
+++ src/API/TooShortWillRead.Crawler/DataSources/DataSourceArticle.cs
@@ -0,0 +1,15 @@
+﻿namespace TooShortWillRead.Crawler.DataSources
+{
+    public class DataSourceArticle
+    {
+        public string InternalDataSourceId { get; set; }
+
+        public string DataSourceName { get; set; }
+
+        public string Header { get; set; }
+
+        public string ImageUrl { get; set; }
+
+        public string Text { get; set; }
+    }
+}
---
+++ src/API/TooShortWillRead.Crawler/Program.cs
@@ -0,0 +1,34 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TooShortWillRead.Crawler.DataSources;
+using TooShortWillRead.Crawler.DataSources.Wikipedia;
+using TooShortWillRead.DAL;
+
+namespace TooShortWillRead.Crawler
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureServices((hostContext, services) =>
+                {
+                    services.AddDbContext<ApplicationDbContext>(options =>
+                        options.UseSqlServer(hostContext.Configuration.GetConnectionString("DefaultConnection")));
+                    services.AddTransient<IDataSource, WikipediaDataSource>();
+                    services.AddHttpClient();
+
+                    services.AddHostedService<Worker>();
+                });
+    }
+}
