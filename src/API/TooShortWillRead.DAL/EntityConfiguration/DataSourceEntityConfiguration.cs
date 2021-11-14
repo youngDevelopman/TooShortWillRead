08cffe84aba56ff6c044f7275cdbcfcd2fccc615
Nazar Mazuryk <Nazar_Mazuryk@epam.com>
2021-11-14 23:43:00 +0200

    Update database schema

---
+++ src/API/TooShortWillRead.DAL/EntityConfiguration/DataSourceEntityConfiguration.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using TooShortWillRead.DAL.Models;
+
+namespace TooShortWillRead.DAL.EntityConfiguration
+{
+    class DataSourceEntityConfiguration : IEntityTypeConfiguration<DataSource>
+    {
+        public void Configure(EntityTypeBuilder<DataSource> builder)
+        {
+            builder.HasKey(entity => entity.Id);
+
+            builder.Property(entity => entity.Name)
+                .HasMaxLength(20)
+                .IsRequired();
+
+            builder.HasIndex(entity => entity.Name)
+                .IsUnique();
+
+            builder.HasData(
+                new DataSource() { Id = 1, Name = "ManuallyCreated" },
+                new DataSource() { Id = 2, Name = "Wikipedia" }
+            );
+        }
+    }
+}
---
+++ src/API/TooShortWillRead.DAL/Migrations/20211114213044_AddDataSourceAwareness.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TooShortWillRead.DAL.Migrations
+{
+    public partial class AddDataSourceAwareness : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "DataSourceId",
+                table: "Articles",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "InternalId",
+                table: "Articles",
+                type: "nvarchar(450)",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "DataSource",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(20)", maxLength: 20, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DataSource", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "DataSource",
+                columns: new[] { "Id", "Name" },
+                values: new object[] { 1, "ManuallyCreated" });
+
+            migrationBuilder.InsertData(
+                table: "DataSource",
+                columns: new[] { "Id", "Name" },
+                values: new object[] { 2, "Wikipedia" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Articles_DataSourceId_InternalId",
+                table: "Articles",
+                columns: new[] { "DataSourceId", "InternalId" },
+                unique: true,
+                filter: "[InternalId] IS NOT NULL");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DataSource_Name",
+                table: "DataSource",
+                column: "Name",
+                unique: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Articles_DataSource_DataSourceId",
+                table: "Articles",
+                column: "DataSourceId",
+                principalTable: "DataSource",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Articles_DataSource_DataSourceId",
+                table: "Articles");
+
+            migrationBuilder.DropTable(
+                name: "DataSource");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Articles_DataSourceId_InternalId",
+                table: "Articles");
+
+            migrationBuilder.DropColumn(
+                name: "DataSourceId",
+                table: "Articles");
+
+            migrationBuilder.DropColumn(
+                name: "InternalId",
+                table: "Articles");
+        }
+    }
+}
