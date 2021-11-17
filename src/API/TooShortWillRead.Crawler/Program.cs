9fe4ed00284f9fbb576d6801c5b9078e55a6721e
Nazar Mazuryk <Nazar_Mazuryk@epam.com>
2021-11-17 20:02:05 +0200

    Upload images to the azure blob storage

--- src/API/TooShortWillRead.Crawler/Program.cs
+++ src/API/TooShortWillRead.Crawler/Program.cs
@@ -8,6 +8,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using TooShortWillRead.Crawler.DataSources;
 using TooShortWillRead.Crawler.DataSources.Wikipedia;
+using TooShortWillRead.Crawler.Services;
 using TooShortWillRead.DAL;
 
 namespace TooShortWillRead.Crawler
@@ -26,6 +27,7 @@ namespace TooShortWillRead.Crawler
                     services.AddDbContext<ApplicationDbContext>(options =>
                         options.UseSqlServer(hostContext.Configuration.GetConnectionString("DefaultConnection")));
                     services.AddTransient<IDataSource, WikipediaDataSource>();
+                    services.AddTransient<IPicturesStorage, AzureBlobPictureStorage>();
                     services.AddHttpClient();
 
                     services.AddHostedService<Worker>();
---
+++ src/API/TooShortWillRead.Crawler/Services/AzureBlobPictureStorage.cs
@@ -0,0 +1,36 @@
+﻿using Azure.Storage.Blobs;
+using System;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+using System.Collections.Generic;
+
+namespace TooShortWillRead.Crawler.Services
+{
+    public class AzureBlobPictureStorage : IPicturesStorage
+    {
+        private readonly BlobContainerClient _blobContainerClient;
+        public AzureBlobPictureStorage(IConfiguration configuration)
+        {
+            string connectionString = configuration.GetConnectionString("PictureStorage");
+            string containerName = configuration.GetSection("ArticlePicturesContainerName").Value;
+            var blobServiceClient = new BlobServiceClient(connectionString);
+            _blobContainerClient = blobServiceClient.GetBlobContainerClient(containerName);
+        }
+
+        public async Task UploadImage(Uri uri)
+        {
+            var filename = Path.GetFileName(uri.LocalPath);
+            var blobClient = _blobContainerClient.GetBlobClient(filename);
+            await blobClient.StartCopyFromUriAsync(uri);
+        }
+
+        public async Task UploadImages(List<Uri> uris)
+        {
+            foreach (var uri in uris) 
+            {
+                await this.UploadImage(uri);
+            }
+        }
+    }
+}
