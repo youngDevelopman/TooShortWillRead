abde666c3c3d2ac8654f92401b666373464b606d
Nazar Mazuryk <Nazar_Mazuryk@epam.com>
2022-02-15 15:24:49 +0200

    Increase an amount of characters for the header

---
+++ src/API/TooShortWillRead.DAL/Migrations/20220215131028_IncreaseHeaderLength.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TooShortWillRead.DAL.Migrations
+{
+    public partial class IncreaseHeaderLength : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Header",
+                table: "Articles",
+                type: "nvarchar(60)",
+                maxLength: 60,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(50)",
+                oldMaxLength: 50);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Header",
+                table: "Articles",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(60)",
+                oldMaxLength: 60);
+        }
+    }
+}
