3f99c932b25b4d6ca4029e8859b4ebb4d6fb3e5c
Nazar Mazuryk <31933374+youngDevelopman@users.noreply.github.com>
2022-07-02 19:17:24 +0300

    Add bottom tab (#28)
    * Update to build number 2
    
    * Add bottom tab
    
    * Add Ionicons
    Display solid icons when selected
    
    * Remove redundant code
    Update build version

--- src/UI/TooShortWillRead/App.js
+++ src/UI/TooShortWillRead/App.js
@@ -8,20 +8,67 @@
 
 import React from 'react';
 import type { Node } from 'react';
+import { Text, View, Button } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 import ArticleScreen from './screens/ArticleScreen';
 import BrowserScreen from './screens/BrowserScreen';
 
-const Stack = createNativeStackNavigator();
+const ArticleStack = createNativeStackNavigator();
+
+function ArticleStackScreen() {
+  return (
+    <ArticleStack.Navigator>
+      <ArticleStack.Screen name="Article" component={ArticleScreen} options={{ headerShown: false }} />
+      <ArticleStack.Screen name="Browser" component={BrowserScreen} options={{ headerShown: false }} />
+    </ArticleStack.Navigator>
+  )
+}
+
+const Tab = createBottomTabNavigator();
+
+function FavouriteArticlesScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Favourite Articles!</Text>
+    </View>
+  );
+}
 
 const App: () => Node = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Article" component={ArticleScreen} options={{headerShown:false}}/>
-        <Stack.Screen name="Browser" component={BrowserScreen} options={{headerShown:false}}/>
-      </Stack.Navigator>
+      <Tab.Navigator screenOptions={({ route }) => ({
+        tabBarStyle: {
+          backgroundColor: 'rgba(34,36,40,1)',
+        }
+      })}>
+        <Tab.Screen name="Article" component={ArticleStackScreen}
+          options={{
+            headerShown: false,
+            tabBarShowLabel: false,
+            tabBarIcon: ({ focused, color, size }) => {
+              let iconName = 'book-outline'
+              if(focused){
+                iconName = 'book'
+              }
+              return <Ionicons name={iconName} color='dodgerblue' size={25} />
+            }
+          }} />
+        <Tab.Screen name="Favourites" component={FavouriteArticlesScreen}
+          options={{
+            tabBarShowLabel: false,
+            tabBarIcon: ({ focused, color, size }) => {
+              let iconName = 'star-outline'
+              if(focused){
+                iconName = 'star'
+              }
+              return <Ionicons name={iconName} color='dodgerblue' size={25} />
+            },
+          }} />
+      </Tab.Navigator>
     </NavigationContainer>
   );
 };
