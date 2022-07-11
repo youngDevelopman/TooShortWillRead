53da0d514aee9fc1a90f009bd5c834026fb807bf
youngDevelopman <nazarmazurik99@gmail.com>
2022-07-11 17:46:32 +0300

    Remove unused services

--- src/UI/TooShortWillRead/services/ArticleService.js
+++ src/UI/TooShortWillRead/services/ArticleService.js
@@ -1,12 +1,7 @@
 import Article from "../models/Article";
-import ArticlesAsyncStorage from "./ArticlesAsyncStorage";
 import Config from "react-native-config";
 
 const TSWR_BASE_URL = Config.TSWR_BASE_URL;
-
-const LOAD_ARTICLES_ATTEMPS_TRESHOLD = Config.LOAD_ARTICLES_ATTEMPS_TRESHOLD;
-const RUN_READ_ARTICLES_CLEANUP_TRESHOLD = Config.RUN_READ_ARTICLES_CLEANUP_TRESHOLD;
-
 const ArticleService = {
     getRandomArticleIdAsync: async function () {
         const randomArticleIdResponse = await fetch(`${TSWR_BASE_URL}/api/article/random/id`);
@@ -30,27 +25,6 @@ const ArticleService = {
         const articlesCountJson = await articlesCountResponse.json();
         const articlesCount = articlesCountJson.count;
         return articlesCount;
-    },
-    loadNewArticleAsync: async function () {
-        let isUnique = false;
-        let attempts = 0;
-        let articleId;
-        do {
-            articleId = await this.getRandomArticleIdAsync();
-            if (!await ArticlesAsyncStorage.articleExists(articleId)) {
-                isUnique = true;
-            }
-            console.log('attemp number', attempts)
-            attempts++;
-        } while (!isUnique && attempts <= LOAD_ARTICLES_ATTEMPS_TRESHOLD);
-
-        if (attempts >= RUN_READ_ARTICLES_CLEANUP_TRESHOLD) {
-            const articlesCount = await this.getArticlesCountAsync();
-            await ArticlesAsyncStorage.cleanArticles(articlesCount);
-        }
-
-        await ArticlesAsyncStorage.addArticle(articleId);
-        return this.getArticleById(articleId);
     }
 }
 
