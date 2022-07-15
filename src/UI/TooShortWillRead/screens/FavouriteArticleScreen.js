c0d9ab53741921c0d01e8d32e2ab4a50bac1ea95
youngDevelopman <nazarmazurik99@gmail.com>
2022-07-16 00:13:37 +0300

    Implement loading animation for the main article screen

--- src/UI/TooShortWillRead/screens/FavouriteArticleScreen.js
+++ src/UI/TooShortWillRead/screens/FavouriteArticleScreen.js
@@ -78,7 +78,7 @@ export default function FavouriteArticleScreen({ route, navigation }) {
     }
 
     return (
-        <ArticleScreen
+         <ArticleScreen
             article={article}
             isLoading={isLoading}
             header={<Header 
